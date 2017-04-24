586ae35b953b6d0bf5ef60e60b2dfcbae0efc0ef
Loic Teixeira <loic@springload.co.nz>
2017-04-25 11:13:45 +1200

    Initial brew.sh

---
+++ brew.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Instal modules
+brew install bash-completion
+brew install git
