6d93e0408ebbcfc362459f18ce43f2a8f8a5eca9
Loïc Teixeira <hello@lta.me>
2024-05-12 16:38:22 +0200

    Install asdf plugins

---
+++ home/.chezmoiscripts/darwin/run_onchange_after_install-asdf-plugins.sh
@@ -0,0 +1,9 @@
+#! /bin/zsh
+
+set -ev pipefail
+
+asdf plugin add golang "https://github.com/asdf-community/asdf-golang.git"
+asdf plugin add nodejs "https://github.com/asdf-vm/asdf-nodejs.git"
+asdf plugin add python "https://github.com/asdf-community/asdf-python.git"
+
+asdf plugin update --all
